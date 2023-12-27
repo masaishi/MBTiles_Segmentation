30e3408c71f79fbb858181762b3eb16fc7f4124f
masaishi <mwishiha@ucsc.edu>
2023-12-26 22:31:46 -0700

    Update files

--- setup.py
+++ setup.py
@@ -1,7 +1,6 @@
 from setuptools import setup, find_packages
 
 install_requires = [
-	'argparse',
 	'pandas',
 	'mapbox_vector_tile',
 	'Pillow'
@@ -16,7 +15,7 @@ setup(
 	long_description_content_type='text/markdown',
 	license='MIT License',
 	url='https://github.com/masaishi/MBTiles_Segmentation',
-	version='0.1',
+	version='0.1.1',
 	python_requires='>=3.6',
 	install_requires=install_requires,
 	package_dir={"": "src"},
